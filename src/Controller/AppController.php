ec0b2d7692b855209062fc82ac1f3eb0010e3880
Takeshi Ueda <uedatakeshi@gmail.com>
2014-11-28 19:18:46 +0900

    added authentication feature

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -26,6 +26,21 @@ use Cake\Controller\Controller;
  */
 class AppController extends Controller {
 
+    public function initialize() {
+        $this->loadComponent('Flash');
+        $this->loadComponent('Auth', [
+            'loginRedirect' => [
+                'controller' => 'Articles',
+                'action' => 'index'
+            ],
+            'logoutRedirect' => [
+                'controller' => 'Pages',
+                'action' => 'display',
+                'home'
+            ]
+        ]);
+    }
+
 /**
  * Components this controller uses.
  *
--- src/Model/Entity/User.php
+++ src/Model/Entity/User.php
@@ -2,6 +2,7 @@
 namespace App\Model\Entity;
 
 use Cake\ORM\Entity;
+use Cake\Auth\DefaultPasswordHasher;
 
 /**
  * User Entity.
@@ -19,4 +20,7 @@ class User extends Entity {
 		'role' => true,
 	];
 
+  protected function _setPassword($password) {
+    return (new DefaultPasswordHasher)->hash($password);
+  }
 }
